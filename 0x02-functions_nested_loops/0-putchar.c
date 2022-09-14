6affcdeb6b1a23b9b02c6245bcb8cd5614c73e2a
Michael Amuah <mkdamuah@hotmail.com>
2022-09-14 00:23:05 +0000

    Initial commit, README + task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+*main - Prints _putchar
+*Description:Standard libraries are disallowed 
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char _putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != "\0")
+	{
+	_putchar9putchar[i]);
+	i++;
+	}
+
+	return (0);
+}
