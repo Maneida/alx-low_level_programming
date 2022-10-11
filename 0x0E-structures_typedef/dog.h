8fe675c2aec2958cb2ad33709df450cdf223d95d
Michael Amuah <mkdamuah@hotmail.com>
2022-10-11 00:33:12 +0000

    Initial commit, Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,12 +4,11 @@
 
 /**
 * struct dog - dog class
-*
 * @name: Name of the dog
-*
 * @age: Age of the dog
-*
 * @owner: Owner of the dog
+*
+*Description:struct called 'dog' which stores its name, age and owner
 */
 
 struct dog{
