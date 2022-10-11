1d0987c5d83998f04ed7b0dab7581addb90f03c2
Michael Amuah <mkdamuah@hotmail.com>
2022-10-11 08:04:23 +0000

    Task 2 correction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,8 +14,20 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n", d->name);
+		if (d == NULL)
+		{
+			printf("Name: (nil)\n")
+		} else
+		{
+			printf("Name: %s\n", d->name);
+		}
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		if (d == NULL)
+		{
+			printf("Owner: (nil)\n")
+		} else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
 	}
 }
