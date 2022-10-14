ff379203af6335beba8d13811c0b482c026a4110
Michael Amuah <mkdamuah@hotmail.com>
2022-10-14 13:01:33 +0000

    task 1 commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes function as parameter for each array element
+ * @array: input integer array
+ * @size: size of the array
+ * @action: pointer to function that needs to be used
+ *
+ * Return: no return
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+		for (i = 0, i < size, i++)
+		{
+			action(array[i]);
+		}
+}
+
