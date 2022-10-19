a5ce8480b6f4b01880347653fbc0c1d9965db143
Michael Amuah <mkdamuah@hotmail.com>
2022-10-19 00:08:46 +0000

    task 12 incomplete

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two-digit numbers
+ * The two numbers are separated by a space
+ * number combinations separated by a comma
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, num1_1, num1_2, num2_1, num2_2;
+
+	num1_1 = '0';
+	for (i = 0; i < 10; ++i)
+	{
+		
