a22ece7bf675d73af230452a8c0d5e9db6872be3
Michael Amuah <mkdamuah@hotmail.com>
2022-10-19 13:37:19 +0000

    task 12 complete

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,25 +13,25 @@ int main(void)
 	int i, num1_1, num1_2, num2_1, num2_2;
 
 	num1_1 = '0';
-	for (i = 0; i < 10; ++i)
+	for (i = 0; i < 10; ++i) /* tens of num1 */
 	{
 		num1_2 = '0';
-		while (num1_2 <= '9')
+		while (num1_2 <= '9') /* ones of num1 */
 		{
-			num2_1 = '0';
+			num2_1 = num1_1; /* tens of num2 */
 			while (num2_1 <= '9')
 			{
-				num2_2 = '0';
+				num2_2 = num1_2 + 1;
 				while (num2_2 <= '9')
 				{
-					if (num2_2 != num1_2) /* edit later */
-					{
-						putchar(num1_1);
-						putchar(num1_2);
-						putchar(' ');
+					putchar(num1_1);
+					putchar(num1_2);
+					putchar(' ');
 
-						putchar(num2_1);
-						putchar(num2_2);
+					putchar(num2_1);
+					putchar(num2_2);
+					if (num1_1 != '9' || num1_2 != '8' || num2_1 != '9' || num2_2 != '9')
+					{
 						putchar(',');
 						putchar(' ');
 					}
