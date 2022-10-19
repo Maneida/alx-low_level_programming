17b5daa3a6fa2e343826e648e31f2133f610f563
Michael Amuah <mkdamuah@hotmail.com>
2022-10-19 01:01:43 +0000

    task 12 incomplete

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,4 +15,36 @@ int main(void)
 	num1_1 = '0';
 	for (i = 0; i < 10; ++i)
 	{
-		
+		num1_2 = '0';
+		while (num1_2 <= '9')
+		{
+			num2_1 = '0';
+			while (num2_1 <= '9')
+			{
+				num2_2 = '0';
+				while (num2_2 <= '9')
+				{
+					if (num2_2 != num1_2) /* edit later */
+					{
+						putchar(num1_1);
+						putchar(num1_2);
+						putchar(' ');
+
+						putchar(num2_1);
+						putchar(num2_2);
+						putchar(',');
+						putchar(' ');
+					}
+					++num2_2;
+				}
+				++num2_1;
+			}
+			++num1_2;
+		}
+		++num1_1;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
