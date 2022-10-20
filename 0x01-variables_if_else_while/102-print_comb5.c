362dbe7dac9e345425b73988b79a9b94968d13f4
Michael Amuah <mkdamuah@hotmail.com>
2022-10-20 23:34:05 +0000

    task 12 correction works

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,29 +10,22 @@
 
 int main(void)
 {
-	int num1_1, num1_2, num2_1, num2_2;
+	int num1, num2;
 
-	for (num1_1 = '0'; num1_1 <= '9'; ++num1_1) /* tens of num1 */
+	for (num1 = 0; num1 <= 99; ++num1) /* tens of num1 */
 	{
-		for (num1_2 = '0'; num1_2 <= '9'; ++num1_2) /* ones of num1 */
+		for (num2 = num1 + 1; num2 <= 99; ++num2) /* ones of num1 */
 		{
-			for (num2_1 = num1_1; num2_1 <= '9'; ++num2_1) /* tens of num2 */
-			{
-				for (num2_2 = num1_2 + 1; num2_2 <= '9'; ++num2_2)
-				{
-					putchar(num1_1);
-					putchar(num1_2);
-					putchar(' ');
+			putchar('0' + (num1 / 10));
+			putchar('0' + (num1 % 10));
+			putchar(' ');
 
-					putchar(num2_1);
-					putchar(num2_2);
-					if (num1_1 != '9' || num1_2 != '8' || num2_1 != '9' || num2_2 != '9')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				num2_2 = '0';
+			putchar('0' + (num2 / 10));
+			putchar('0' + (num2 % 10));
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
---
+++ 0x01-variables_if_else_while/102-test.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two-digit numbers
+ * The two numbers are separated by a space
+ * number combinations separated by a comma
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num1_1, num1_2, num2_1, num2_2;
+
+	for (num1_1 = '0'; num1_1 <= '9'; ++num1_1) /* tens of num1 */
+	{
+		for (num1_2 = '0'; num1_2 <= '9'; ++num1_2) /* ones of num1 */
+		{
+			for (num2_1 = num1_1; num2_1 <= '9'; ++num2_1) /* tens of num2 */
+			{
+				for (num2_2 = num1_2 + 1; num2_2 <= '9'; ++num2_2)
+				{
+					putchar(num1_1);
+					putchar(num1_2);
+					putchar(' ');
+
+					putchar(num2_1);
+					putchar(num2_2);
+					if (num1_1 != '9' || num1_2 != '8' || num2_1 != '9' || num2_2 != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				num2_2 = '0';
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
