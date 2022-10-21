bae604e43871767857463979d1549b6390e1e78e
Michael Amuah <mkdamuah@hotmail.com>
2022-10-21 09:56:01 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10x
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (alpha = 'a'; alpha <= 'z'; ++alpha)
+			_putchar(alpha);
+		_putchar('\n');
+	}
+}
