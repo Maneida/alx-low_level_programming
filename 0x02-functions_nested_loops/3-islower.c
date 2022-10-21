86b557701805d32cbb73fccc0183fa5e84029f98
Michael Amuah <mkdamuah@hotmail.com>
2022-10-21 12:35:33 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower(int c) -  checks for lower case character
+ *
+ * Return: 1 if  cis lower and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return(0);
+}
