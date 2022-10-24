5bc7c6d1d9e7cac743dad5e5eb1f92f8d0a65e35
Michael Amuah <mkdamuah@hotmail.com>
2022-10-24 11:06:47 +0000

    task 5 correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,15 @@
 
 void more_numbers(void)
 {
-	int i;
+	int i,j;
 
 	for (i = 0; i < 10; ++i)
 	{
-		for (i = 0; i <= 14; ++i)
+		for (j = 0; j <= 14; ++j)
 		{
 			if (i > 9)
-				_putchar('0' + (i / 10));
-			_putchar('0' + (i % 10));
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
