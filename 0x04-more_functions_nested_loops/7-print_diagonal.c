c077808500645c7554fd7ce4c10866cb83ecd857
Michael Amuah <mkdamuah@hotmail.com>
2022-10-24 11:24:39 +0000

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws adiagonal line ont the terminal
+ * @n: number of times char '\' should be printed
+ *
+ * Return: no return
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (n > 0)
+	{
+		for (i = 0; i < n; ++i)
+		{
+			for (j = 0; j < n - 1; ++j)
+				_putchar(' ');
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+}
