449c3c95809b26afc4ad67a609de0127aefbfa1a
Michael Amuah <mkdamuah@hotmail.com>
2022-10-25 01:36:06 +0000

    task 101 corrections

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,23 +12,23 @@ void print_number(int n)
 {
 	int div;
 
-        if (n < 0) /* print negative sign for negative numbers */
-        {
-                n *= -1;
-                _putchar('-');
-        }
+	if (n < 0) /* print negative sign for negative numbers */
+	{
+		n *= -1;
+		_putchar('-');
+	}
 
-        div = 1; /* find biggest divisor by exponents of 10 */
-        while (n / div != 0)
-                div *= 10;
+	div = 1; /* find biggest divisor by exponents of 10 */
+	while (n / div != 0)
+		div *= 10;
 	printf("%i", div);
 
-        while (div >= 10) /* loop through and print from biggest digit up to tens */
-        {
-                if (n / div != 0)
-                        _putchar('0' + ((n / div) % 10));
-                div /= 10;
-        }
+	while (div >= 10) /* loop through and print from biggest digit up to tens */
+	{
+		if (n / div != 0)
+			_putchar('0' + ((n / div) % 10));
+		div /= 10;
+	}
 
 	if (n != 0) /* print digit for ones */
 		_putchar('0' + (n % 10));
