199b07c63b34d1d3bc2848027ef0b78f344fe441
Michael Amuah <mkdamuah@hotmail.com>
2022-11-26 12:13:07 +0000

    task 0 redo

---
+++ 0x05-pointers_arrays_strings/redo/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - updates pointer value to 98
+ * @n: pointer to integer to be updated
+ *
+ * Return: void on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
