035ae742b88e7416916e864e88c9ceea6619e883
Michael Amuah <mkdamuah@hotmail.com>
2022-12-08 22:00:43 +0000

    bilk task commit + corrections

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,18 +19,25 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(struct dlistint_s));
 
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+
 	if (*head == NULL)
 	{
 		*head = new;
 		new->next = NULL;
 		new->prev = NULL;
+		return (new);
 	}
-	
-	new->n = n;
-	new->next = *head;
-	new->prev = NULL;
-	(*head)->prev = new;
-	*head = new;
+	else
+	{
+		new->next = *head;
+		new->prev = NULL;
+		(*head)->prev = new;
+		*head = new;
+	}
 
 	return (new);
 }
