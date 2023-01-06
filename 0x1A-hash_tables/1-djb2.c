55572697c56742aa44b8870a14461a9514861982
Michael Amuah <mkdamuah@hotmail.com>
2023-01-06 02:50:32 +0000

    bulk hash tables commit

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implement djb2 algorithm (one of best string hash functions)
+ * source code copied from https://gist.github.com/papamuziko/7bb52dfbb859fdffc4bd0f95b76f71e8
+ *
+ * @str: value
+ * Return: key
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
+}
